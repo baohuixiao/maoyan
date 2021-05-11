412f17c2d8a56691ab7125777fb18fb53d9caf89
baohuixiao <17705032736@163.com>
2021-05-11 20:29:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,93 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>猫眼电影</header>
+    <main></main>
+    <nav>
+      <ul>
+        <li class="active">
+          <b>&#xe8ae;</b>
+          <span>电影</span>
+        </li>
+        <li>
+          <b>&#xe8c0;</b>
+          <span>影院</span>
+        </li>
+        <li>
+          <b>&#xe606;</b>
+          <span>我的</span>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello",
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@font-face {
+  font-family: iconfont-nav;
+  src: url('./assets/icons/iconfont-nav.ttf') format('truetype');
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+
+  header {
+    background-color: #cd4c42;
+    color: #fff;
+    height: 0.44rem;
+    font-size: 0.16rem;
+    font-weight: bold;
+    text-align: center;
+    line-height: 0.44rem;
+  }
+
+  main {
+    flex: 1;
+  }
+
+  nav {
+    height: 0.5rem;
+    border-top: 1px solid #ededed;
+
+    ul {
+      display: flex;
+      height 100%
+
+      li {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: iconfont-nav;
+        color: #999;
+
+        b {
+          font-size: 0.26rem;
+          font-weight: normal;
+          line-height: 0.25rem;
+        }
+
+        span {
+          font-size: 0.12rem;
+          transform: scale(0.9);
+        }
+      }
+
+      .active {
+        color: #cd4c42;
+      }
+    }
+  }
+}
 </style>
